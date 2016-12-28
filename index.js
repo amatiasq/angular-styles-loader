add7558ba5846956b0f9d3f419afe69affd990be
Hiten Patel <hitenpatel01@gmail.com>
2016-12-28 12:42:55 -0500

    Returning string instead of array

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = function(content) {
   this.cacheable();
-  return content + '\nmodule.exports = [module.exports[0][1]]';
+  return content + '\nmodule.exports = module.exports[0][1]';
 };
